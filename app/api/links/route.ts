20209bde99cf0056a534e8d384bd61284bcda4c4
Steven Tey <stevensteel97@gmail.com>
2025-03-14 12:49:05 -0700

    add parseRequestBody

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -3,15 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = withAxiom(
   async (req: NextRequest) => {
-    try {
-      const { url } = await req.json();
-      return NextResponse.json({ message: url }, { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ message: error }, { status: 500 });
-    }
+    const { url } = await parseRequestBody(req);
+    return NextResponse.json({ message: url }, { status: 200 });
   },
   {
     logRequestDetails: ["nextUrl", "body"],
   }
 );
+
+const parseRequestBody = async (req: Request) => {
+  try {
+    return await req.json();
+  } catch (e) {
+    console.error(e);
+    throw new Error(
+      `Invalid JSON format in request body. Please ensure the request body is a valid JSON object. ${e}`
+    );
+  }
+};
