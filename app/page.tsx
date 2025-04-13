12570c59947319a6253bbae7574b7f7b28b493ee
Steven Tey <stevensteel97@gmail.com>
2025-04-12 21:21:42 -0700

    test og avatar api cors

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
+        <img
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="https://api.dub.co/og/avatar?seed=debug-next-axiom"
+          alt="Avatar"
+          width={128}
+          height={128}
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
